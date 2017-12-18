7a908769313b42a73f86d524da66eac38d424904
J Gregory <john@johngregory.me.uk>
2017-12-18 12:18:19 +0000

    Refactor hooks and handle "ping" gracefully

--- main.go
+++ main.go
@@ -1,21 +1,17 @@
 package main
 
 import (
-	"crypto/hmac"
-	"crypto/sha1"
-	"crypto/subtle"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"strings"
 	"syscall"
 	"time"
 
 	"github.com/ONSdigital/git-diff-check-service/api"
+	"github.com/ONSdigital/git-diff-check-service/githook"
 	"github.com/ONSdigital/git-diff-check-service/signals"
 	"github.com/ONSdigital/git-diff-check/diffcheck"
 
@@ -29,54 +25,7 @@ var (
 )
 
 const (
-	githubURL       = "https://api.github.com"
-	signaturePrefix = "sha1="
-)
-
-type (
-	// GithubUser represents a github user. This will be embedded in other
-	// returned data. However, not all fields will always be filled in.
-	GithubUser struct {
-		Name      string `json:"name,omitempty"`
-		Email     string `json:"email,omitempty"`
-		Login     string `json:"login,omitempty"`
-		ID        int    `json:"id,omitempty"`
-		AvatarURL string `json:"avatar_url,omitempty"`
-	}
-
-	// PushEvent is the data received from a github "push" webhook
-	PushEvent struct {
-		Ref     string `json:"id"`
-		Commits []struct {
-			ID string `json:"id"`
-		} `json:"commits"`
-		Repository struct {
-			ID       int        `json:"id"`
-			Name     string     `json:"name"`
-			FullName string     `json:"full_name"`
-			Owner    GithubUser `json:"owner"`
-		}
-		Pusher GithubUser `json:"pusher"`
-	}
-
-	// Commit is a single repository commit
-	Commit struct {
-		URL   string `json:"url"`
-		SHA   string `json:"sha"`
-		Files []struct {
-			Filename  string `json:"filename"`
-			Additions int    `json:"additions"`
-			Deletions int    `json:"deletions"`
-			Changes   int    `json:"changes"`
-			Status    string `json:"status"`
-			Patch     string `json:"patch"`
-		} `json:"files"`
-		Commit struct {
-			Author    GithubUser `json:"author"`
-			Committer GithubUser `json:"committer"`
-		} `json:"commit"`
-		Message string `json:"message"`
-	}
+	githubURL = "https://api.github.com"
 )
 
 func main() {
@@ -120,44 +69,37 @@ func main() {
 
 func setupRoutes(r *mux.Router) {
 	r.HandleFunc("/push", pushHandler)
-	r.HandleFunc("/healthcheck", healthHandler)
-}
-
-func healthHandler(rw http.ResponseWriter, r *http.Request) {
-	rw.Header().Set("Content-Type", "application/json")
-	rw.Write([]byte(`{"healthy":true}`))
-	rw.WriteHeader(http.StatusOK)
-	return
+	r.HandleFunc("/healthcheck", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Set("Content-Type", "application/json")
+		rw.Write([]byte(`{"healthy":true}`))
+		rw.WriteHeader(http.StatusOK)
+		return
+	})
 }
 
 func pushHandler(rw http.ResponseWriter, r *http.Request) {
-	log.Println(`event="Received push event"`)
-
-	payload, err := parseHook(r, secret)
+	event, err := githook.Parse(r, secret)
 	if err != nil {
 		log.Printf(`event="Error parsing hook" error="%v"`, err)
 		api.WriteProblemResponse(api.Problem{
-			Title:  "Request body unreadable",
-			Status: http.StatusInternalServerError,
+			Title:  "Problem parsing request body",
+			Status: http.StatusBadRequest,
+			Detail: err.Error(),
 		}, rw)
 		return
 	}
 
-	var pushEvent PushEvent
-	if err := json.Unmarshal(payload, &pushEvent); err != nil {
-		log.Printf(`event="Error parsing hook payload" error="%v"`, err)
-		api.WriteProblemResponse(api.Problem{
-			Title:  "Failed to unmarshal payload",
-			Status: http.StatusInternalServerError,
-		}, rw)
-		return
-	}
-
-	for _, commit := range pushEvent.Commits {
-		// Warning: Be careful if you try to run this under a lambda type (e.g.
-		// deploying with apex) as these go routines will probably be killed as
-		// soon as the parent function returns.
-		go checkCommit(pushEvent.Repository.FullName, commit.ID)
+	switch e := event.(type) {
+	case *githook.PingEvent:
+		log.Printf(`event="Received hook event" type="ping" repo="%s"`, e.Repository.FullName)
+	case *githook.PushEvent:
+		log.Printf(`event="Received hook event" type="push" repo="%s"`, e.Repository.FullName)
+		for _, commit := range e.Commits {
+			// Warning: Be careful if you try to run this under a lambda type
+			// (e.g. deploying with apex) as these go routines will probably be
+			// killed as soon as the parent function returns.
+			go checkCommit(e.Repository.FullName, commit.ID)
+		}
 	}
 
 	rw.WriteHeader(http.StatusOK)
@@ -198,7 +140,7 @@ func checkCommit(repo, sha string) {
 	// TODO More robust checking that info returned is valid
 	// ...
 
-	var c Commit
+	var c githook.Commit
 	if err := json.Unmarshal(body, &c); err != nil {
 		log.Printf(`event="Failed to parse commit info" %s error="%v"`, bindLog, err)
 		// TODO need to determine what to do if this fails
@@ -222,42 +164,8 @@ func checkCommit(repo, sha string) {
 		return
 	}
 
-	log.Printf(`event="No issues found in commit" %s`, bindLog)
 	return
 
 	// TODO - Need to actually do something sensible with the reports - like alert someone!
 	// TODO - Though splunk et al could be used in the short term to alert on logged above
-}
-
-func parseHook(r *http.Request, secret []byte) ([]byte, error) {
-	var signature string
-	if signature = r.Header.Get("x-hub-signature"); !strings.HasPrefix(signature, signaturePrefix) {
-		return nil, errors.New("missing x-hub-signature")
-	}
-	signature = strings.TrimLeft(signature, signaturePrefix)
-
-	var event string
-	if event = r.Header.Get("x-github-event"); event != "push" {
-		return nil, errors.New("missing or non-push x-github-event")
-	}
-
-	payload, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	// Use ConstantTimeCompare to mitigate possible timing attacks if
-	// we were to use plain ==
-	if subtle.ConstantTimeCompare(signPayload(payload, secret), []byte(signature)) == 1 {
-		return payload, nil
-	}
-	return nil, errors.New("signature check failed")
-}
-
-func signPayload(payload, secret []byte) []byte {
-	computed := hmac.New(sha1.New, secret)
-	computed.Write(payload)
-	// Return the hex encoded representation of the signature so it can be
-	// plugged directly into a header (and compared with an existing header)
-	return []byte(fmt.Sprintf("%x", computed.Sum(nil)))
 }
